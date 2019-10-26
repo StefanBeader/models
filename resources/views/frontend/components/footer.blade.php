eec817aee414903ca4e8e8d0d1eb375ceb12b456
Stefan Beader <stefanbeader@Stefans-MacBook-Pro.local>
2019-10-26 08:05:01 +0200

    social media links

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('frontend/img/logo.jpg') }}" alt="">
     </div>
     <div class="social-icons">
-        <a class="icon" href="https://facebook.com">
+        <a class="icon" href="https://facebook.com" target="_blank" rel="noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1"
                  x="0px" y="0px" viewBox="0 0 96.124 96.123"
                  style="enable-background:new 0 0 96.124 96.123;" xml:space="preserve">
@@ -13,7 +13,7 @@
                     </g>
             </svg>
         </a>
-        <a class="icon" href="https://facebook.com">
+        <a class="icon" href="https://instagram.com/d_model_management" target="_blank" rel="noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 169.063 169.063" style="enable-background:new 0 0 169.063 169.063;" xml:space="preserve">
 <g>
     <path d="M122.406,0H46.654C20.929,0,0,20.93,0,46.655v75.752c0,25.726,20.929,46.655,46.654,46.655h75.752   c25.727,0,46.656-20.93,46.656-46.655V46.655C169.063,20.93,148.133,0,122.406,0z M154.063,122.407   c0,17.455-14.201,31.655-31.656,31.655H46.654C29.2,154.063,15,139.862,15,122.407V46.655C15,29.201,29.2,15,46.654,15h75.752   c17.455,0,31.656,14.201,31.656,31.655V122.407z"/>
@@ -22,7 +22,7 @@
 </g>
 </svg>
         </a>
-        <a class="icon" href="https://facebook.com">
+        <a class="icon" href="https://youtube.com" target="_blank" rel="noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 310 310" style="enable-background:new 0 0 310 310;" xml:space="preserve">
 <g id="XMLID_822_">
     <path id="XMLID_823_" d="M297.917,64.645c-11.19-13.302-31.85-18.728-71.306-18.728H83.386c-40.359,0-61.369,5.776-72.517,19.938   C0,79.663,0,100.008,0,128.166v53.669c0,54.551,12.896,82.248,83.386,82.248h143.226c34.216,0,53.176-4.788,65.442-16.527   C304.633,235.518,310,215.863,310,181.835v-53.669C310,98.471,309.159,78.006,297.917,64.645z M199.021,162.41l-65.038,33.991   c-1.454,0.76-3.044,1.137-4.632,1.137c-1.798,0-3.592-0.484-5.181-1.446c-2.992-1.813-4.819-5.056-4.819-8.554v-67.764   c0-3.492,1.822-6.732,4.808-8.546c2.987-1.814,6.702-1.938,9.801-0.328l65.038,33.772c3.309,1.718,5.387,5.134,5.392,8.861   C204.394,157.263,202.325,160.684,199.021,162.41z"/>
