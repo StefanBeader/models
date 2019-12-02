52413718304ce41583949bb26119564c4f207e3f
Stefan Beader <stefanbeader@Stefans-MacBook-Pro.local>
2019-12-02 18:29:47 +0100

    changed link to review model to edit link

--- resources/views/mails/pending_model.blade.php
+++ resources/views/mails/pending_model.blade.php
@@ -6,7 +6,7 @@
            <h1 class="text-center">New Model Request</h1>
            <h4 class="text-center">{{ $mannequinName }}</h4>
 
-           <a href="{{ URL::to('/models/review/' . $mannequinId) }}" class="btn btn-primary">Review</a>
+           <a href="{{ URL::to('/models/' . $mannequinId . '/edit') }}" class="btn btn-primary">Review</a>
        </section>
     </div>
 @endsection
